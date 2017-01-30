76623b89e8897f3aa47bfb26526707c20b086188
David ROCA <david.roca@laposte.fr>
2017-01-30 11:24:25 +0100

    Add Launcher GlassFish Domain 1 & 2 Work

---
+++ WorkSpace_Config/work/start-glassfish - domain2.cmd
@@ -0,0 +1,8 @@
+@REM https://glassfish.java.net/download.html
+cd C:\DevJ2EE\glassfish-4.1.1\bin
+
+@REM SET AS_JAVA=C:\Program Files\Java\jdk1.8.0_111
+
+asadmin start-domain -e -v domain2
+
+pause
