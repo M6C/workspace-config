6b89065ba21624541d7575591610ebd4fd1507fb
M6C <david.roca@free.fr>
2017-02-06 21:20:16 +0100

    Add Home Cmd

---
+++ WorkSpace_Config/home/start-glassfish - domain2.cmd
@@ -0,0 +1,4 @@
+@REM https://glassfish.java.net/download.html
+cd D:\DevJ2EE\glassfish-4.1.1\bin
+
+asadmin start-domain -v -e domain2
---
+++ WorkSpace_Config/home/stop-glassfish - domain1.cmd
@@ -0,0 +1,6 @@
+@REM https://glassfish.java.net/download.html
+cd D:\DevJ2EE\glassfish-4.1.1\bin
+
+asadmin stop-domain -e domain1
+
+pause
